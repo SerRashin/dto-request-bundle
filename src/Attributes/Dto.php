8b7e2430da16c796b7a43fa077f4eeb35397af5f
Rashin Sergey <ser@rashin.me>
2023-07-14 13:20:36 +0300

    add resolving from attribute

--- src/Attributes/Dto.php
+++ src/Attributes/Dto.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace Ser\DtoRequestBundle\Attributes;
 
 use Attribute;
+use Ser\DtoRequestBundle\Enum\Source;
 
 #[Attribute(Attribute::TARGET_PARAMETER)]
 class Dto
 {
-    public function __construct()
+    public function __construct(
+    )
     {
     }
 }
---
+++ src/Enum/Source.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ser\DtoRequestBundle\Enum;
+
+enum Source
+{
+    case Query;
+    case Data;
+}
